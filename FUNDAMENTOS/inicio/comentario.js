9f4364171dd140097fa9b4a923ebdc5709ec6617
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-01-02 20:36:05 -0300

    Add files via upload

---
+++ FUNDAMENTOS/inicio/comentario.js
@@ -0,0 +1,6 @@
+// console.log é usado para exibir algo no computador
+console.log("Passo #01");
+console.log("Passo #02"); // mais um exemplo de comentário
+// console.log("Passo #03");
+
+/* Dentro dessas barras e asteriscos também é um exemplo de comentário */
