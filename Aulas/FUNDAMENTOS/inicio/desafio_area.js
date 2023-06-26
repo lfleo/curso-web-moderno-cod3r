ab92832600a145b3c0fe1697a0dd385b5f3a6b29
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-06-26 20:38:20 -0300

    .

---
+++ Aulas/FUNDAMENTOS/inicio/desafio_area.js
@@ -0,0 +1,20 @@
+// Desafio para calcular a área de uma circuferência
+
+// PI = 3.141592
+// PI * raio * raio ou PI * r²
+
+const PI = 3.14;
+let raio = 10;
+let circu = PI * (raio * raio);
+console.log("A circuferência de " + raio + "m" + " é " + circu + "m²");
+
+/*
+Resposta
+
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m².");
+console.log(Math.PI);
+*/
