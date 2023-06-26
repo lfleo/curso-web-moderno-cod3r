992f0f4a64d343a205fc5baa8554a098a98ea193
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-06-26 18:50:00 -0300

    nova pasta

---
+++ Aulas/FUNDAMENTOS/inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,34 @@
+/*
+    
+    Criar dois blocos. O primeiro com números ímpares e o segundo com números pares.
+
+    // Bloco 1
+    1
+    3
+    5
+    7
+    9
+
+    // Bloco 2
+    2
+    4
+    6
+    8
+    10
+*/
+
+{
+    console.log("Número 1");
+    console.log("Número 3");
+    console.log("Número 5");
+    console.log("Número 7");
+    console.log("Número 9");
+}
+
+{
+    console.log("Número 2");
+    console.log("Número 4");
+    console.log("Número 6");
+    console.log("Número 8");
+    console.log("Número 10");
+}
---
+++ Aulas/FUNDAMENTOS/inicio/sentenca_2.js
@@ -0,0 +1,4 @@
+// Assim que se comenta em Javascript
+console.log("Passo #01")
+console.log("Passo #02")
+console.log("Passo #03")
