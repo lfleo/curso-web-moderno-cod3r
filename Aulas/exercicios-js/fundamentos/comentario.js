1d1989bc1d0d958c8fb590fdb55e1ce8d6ad6fc1
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-07-18 21:50:48 -0300

    .

---
+++ Aulas/exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de um linha
+console.logo('Linha 1')
+
+/*
+Comentário de
+múltipla linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * múltiplas linhas
+*/
+console.log('Linha 3')
---
+++ Aulas/exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de cógido");
+
+{
+    {
+        {
+            
+        }
+    }
+}
