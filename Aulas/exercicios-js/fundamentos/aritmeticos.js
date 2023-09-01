425f960e5692cda79189393febfab6322b5ba9a2
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-08-31 23:20:03 -0300

    aritmeticos

---
+++ Aulas/exercicios-js/fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d // SOMA
+const subtracao = d - b // SUBTRAI
+const multiplicacao = a * b // MULTIPLICA
+const divisao = d / a // DIVIDI
+const modulo = a % 2 // RESTO DA DIVISAO
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
