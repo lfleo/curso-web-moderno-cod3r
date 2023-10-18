a650c7dd8a325125a19aec741dbc3c21cb0ba114
Leonardo Fernandes <85836420+lfleo@users.noreply.github.com>
2023-10-17 22:38:31 -0300

    funcaoConstrutura

---
+++ Aulas/exercicios-js/funcao/funcaoContrutora.js
@@ -0,0 +1,31 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    // atributo privado que pertence apenas ao escopo da funçao
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }// responsavel por acrescentar a velocidade atual
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
